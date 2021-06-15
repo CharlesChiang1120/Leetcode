6933afe16ad10806b9136898821a5c047f8fc022
CharlesChiang <charleschiang2343@gmail.com>
2021-06-15 09:21:40 +0800

    Maximum Subarray

---
+++ Easy/Maximum-Subarray.c
@@ -0,0 +1,28 @@
+// int maxSubArray(int* nums, int numsSize){
+//     int max = nums[0];
+//     for (int i = 0; i < numsSize; i++) {
+//         int sum = 0;
+//         for (int j = i; j < numsSize; j ++) {
+//             sum = sum + nums[j];
+//             if ( sum >= max){
+//                 max = sum;
+//             }
+//         }
+//     }
+//     return max;
+// }
+int maxSubArray(int* nums, int size)
+{
+    int sum = 0;
+    int max = INT_MIN;
+    for(int i = 0; i < size; i++)
+    {
+        if(sum >= 0)
+            sum += nums[i];
+        else
+            sum = nums[i];
+        if(sum > max)
+            max = sum;
+    }
+    return max;
+}
